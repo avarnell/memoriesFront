2a40499428823bbc1c759daa8ceb9986198b3054
Andrew <avarnell@ago.org>
2015-11-13 11:22:56 -0700

    added http for all api

--- js/app.js
+++ js/app.js
@@ -2,4 +2,16 @@ var memories = angular.module('memories', ['ngRoute'])
 
 memories.controller('memoriesCtrl', ['$scope', '$http', function($scope, $http){
   $scope.works = 'It Does!'
-}])
+  $http.get('http://g12-varnell-andrew-memories.cfapps.io/api/v1/memories').success(function(response){
+    $scope.memories = response.data
+    console.log(response)
+  })
+
+}]).config(function($routeProvider,$locationProvider){
+  $routeProvider.when('/', {
+    templateUrl: '/home.html',
+    controller: 'memoriesCtrl'
+  })
+  $locationProvider.html5Mode(true)
+})
+
